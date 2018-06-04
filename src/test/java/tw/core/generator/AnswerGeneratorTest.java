db9e8e2d3867530dd7c9abb38e722da06052e5bc
zx <1374040664@qq.com>
2018-06-04 14:49:44 +0800

    add AnswerTest

--- src/test/java/tw/core/generator/AnswerGeneratorTest.java
+++ src/test/java/tw/core/generator/AnswerGeneratorTest.java
@@ -1,9 +1,27 @@
 package tw.core.generator;
 
+import com.google.inject.Injector;
+import org.junit.Assert;
+import org.junit.Test;
+import tw.GuessNumberModule;
+import tw.core.Answer;
+import tw.core.exception.OutOfRangeAnswerException;
+
+import java.util.regex.Pattern;
+
+import static com.google.inject.Guice.createInjector;
+
 /**
  * 在AnswerGeneratorTest文件中完成AnswerGenerator中对应的单元测试
  */
 public class AnswerGeneratorTest {
+    @Test
+    public void should_retrun_a_random_answer_when_invoke_answer_generator() throws OutOfRangeAnswerException {
+        Injector injector = createInjector(new GuessNumberModule());
+        AnswerGenerator answerGenerator = injector.getInstance(AnswerGenerator.class);
+        Answer answer = answerGenerator.generate();
+        Assert.assertTrue(Pattern.matches("[0-9] [0-9] [0-9] [0-9]", answer.toString()));
+    }
 
 }
 
