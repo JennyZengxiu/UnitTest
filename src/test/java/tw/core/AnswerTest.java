61bb1aa79d5513e7155c001e6f825a1c194fc514
zx <1374040664@qq.com>
2018-06-04 11:40:36 +0800

    add answerTest

--- src/test/java/tw/core/AnswerTest.java
+++ src/test/java/tw/core/AnswerTest.java
@@ -1,8 +1,47 @@
 package tw.core;
 
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import tw.core.exception.OutOfRangeAnswerException;
+
+import java.util.Arrays;
+import static org.junit.Assert.*;
+
 /**
  * 在AnswerTest文件中完成Answer中对应的单元测试
  */
 public class AnswerTest {
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
 
+    Answer answer;
+    private Answer rightAnswer = new Answer();
+    {
+        rightAnswer.setNumList(Arrays.asList("1","2","3","4"));
+    }
+
+    @Before
+    public final void before() {
+        answer = new Answer();
+    }
+
+    @Test
+    public void should_return_answer_when_createAnswer() throws Exception {
+        assertEquals(answer.createAnswer("1 2 3 4").toString(), "1 2 3 4");
+    }
+
+    @Test
+    public void should_return_a_correct_record_when_given_an_input_string() {
+        answer.setNumList(Arrays.asList("2", "3", "4", "5"));
+
+        Answer inputAnswer1 = new Answer();
+        inputAnswer1.setNumList(Arrays.asList("5", "3", "4", "1"));
+
+        int[] expectedValue = {2,1};
+        int[] recordValue = answer.check(inputAnswer1).getValue();
+        assertEquals(expectedValue[0],recordValue[0]);
+        assertEquals(expectedValue[1],recordValue[1]);
+    }
 }
